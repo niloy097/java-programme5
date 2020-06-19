cd21288cd61439bdf29c7d6ffd926d62c55b65c8
niloy097 <niloybiswass097@gmail.com>
2020-06-19 23:34:37 +0600

    firs java

---
+++ HWD.java
@@ -0,0 +1,8 @@
+public class HDW {
+
+    public static void main(String[] args) {
+        //Prints "HWD" to the terminal window.
+        System.out.println("HDW");
+        }
+        
+}        
